4e021c4e35601fed264c406ba621d04781ff3751
David Juarez <DavidJuarez@192.168.1.6>
2017-04-03 20:50:18 -0700

    changed opacity to 0 in css file and added fade in effect in script.js file so that elements can fade in when opening application

--- Assets/js/script.js
+++ Assets/js/script.js
@@ -1,4 +1,13 @@
 $(document).ready(function() {
+
+/*-------------------------------------
+| Fade In Content
+-------------------------------------*/
+$('h1').delay(200).fadeTo(1000, 1);
+$('img').delay(800).fadeTo(1000, 1);
+$('input').delay(1500).fadeTo(1000, 1);
+$('button').delay(1500).fadeTo(1000, 1);
+
   $('#search').click(function() {
     var searchTerm = $('#searchTerm').val();
     var url = "https://en.wikipedia.org/w/api.php?action=opensearch&search=" + searchTerm + "&format=json&callback=?";
@@ -13,6 +22,7 @@ $(document).ready(function() {
               // console.log(data[2][0]);
               // console.log(data[3][0]);
               $('#results').html('');
+              $('#results').fadeIn(750);
                     for(var i=0;i<data[1].length;i++) {
                         $('#results').prepend("<li><a href= "+data[3][i]+">"+data[1][i]         +"</a><p>"+data[2][i]+"</p></li>");
                     }
